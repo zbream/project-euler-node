d31cd83927c0e272588eff314cd25d0760f11c6f
Zack Ream <zbream@outlook.com>
2018-08-18 22:21:03 -0400

    feat: problems 52, 55; introducing BigInteger

---
+++ src/052/index.ts
@@ -0,0 +1,41 @@
+export {};
+
+const result = findSmallestPermutedMultiple();
+console.log(result);
+
+function findSmallestPermutedMultiple(): number {
+  for (let i = 1;; i++) {
+    if (isPermutedMultiple(i)) {
+      return i;
+    }
+  }
+}
+
+function isPermutedMultiple(num: number): boolean {
+  const digits = getDigits(num);
+  for (let multiplier = 2; multiplier <= 6; multiplier++) {
+    const multiplierDigits = getDigits(num * multiplier);
+    if (!isEqual(digits, multiplierDigits)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function getDigits(num: number): number[] {
+  const digits = new Array<number>(10).fill(0);
+  while (num > 0) {
+    digits[num % 10]++;
+    num = Math.floor(num / 10);
+  }
+  return digits;
+}
+
+function isEqual(digits1: number[], digits2: number[]): boolean {
+  for (let i = 0; i < 10; i++) {
+    if (digits1[i] !== digits2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
---
+++ src/055/index.ts
@@ -0,0 +1,44 @@
+import BigInteger, { BigNumber } from 'big-integer';
+
+import { isPalindrome } from '../common/palindrome';
+
+const result = numLychrelNumbersBelow(10000, 50);
+console.log(result);
+
+function numLychrelNumbersBelow(upperLimit: number, maxIterations: number): number {
+  let count = 0;
+  for (let i = 1; i < upperLimit; i++) {
+    if (isLychrelNumber(i, maxIterations)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function isLychrelNumber(num: number, maxIterations: number): boolean {
+  for (const term of ReverseAddGenerator(num)) {
+    if (isPalindrome(term.toString())) {
+      return false;
+    }
+    maxIterations--;
+    if (maxIterations === 0) {
+      return true;
+    }
+  }
+  throw new Error();
+}
+
+function* ReverseAddGenerator(seed: BigNumber) {
+  let value = BigInteger(seed as any);
+  while (true) {
+    let trash = value;
+    let reversed = BigInteger.zero;
+    while (!trash.isZero()) {
+      const { quotient, remainder } = trash.divmod(10);
+      reversed = reversed.times(10).plus(remainder);
+      trash = quotient;
+    }
+    value = value.add(reversed);
+    yield value;
+  }
+}
