bc41533ad782264d719e80e6c8573679769ba2af
Zack Ream <zbream@outlook.com>
2024-07-03 00:55:32 -0400

    refactor: avoid string conversion on problem 32
    Runtime from ~2200ms to ~140ms.

--- src/032/index.ts
+++ src/032/index.ts
@@ -3,12 +3,15 @@ import { PermutationGenerator } from '../common/permutations';
 export function main032() {
   const set = new Set<number>();
   let sum = 0;
-  for (const permutation of PermutationGenerator('123456789'.split(''))) {
+  for (const permutation of PermutationGenerator(
+    '123456789'.split('').map(Number)
+  )) {
     for (let i = 1; i <= 7; i++) {
       for (let j = i + 1; j <= 8; j++) {
-        const a = +permutation.slice(0, i).join('');
-        const b = +permutation.slice(i, j).join('');
-        const c = +permutation.slice(j).join('');
+        const a = getNumber(permutation, 0, i);
+        const b = getNumber(permutation, i, j);
+        const c = getNumber(permutation, j, 9);
+        // console.log(`${a}x${b}=${c}`);
         if (a * b === c && !set.has(c)) {
           set.add(c);
           sum += c;
@@ -17,4 +20,13 @@ export function main032() {
     }
   }
   return sum;
+}
+
+function getNumber(arr: readonly number[], start: number, end: number) {
+  let res = 0;
+  for (let i = start; i < end; i++) {
+    res *= 10;
+    res += arr[i];
+  }
+  return res;
 }
